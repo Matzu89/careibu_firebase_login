f70ed30a8356a9a23798dfb22e17f59bfcbda227
Mats De Swart <mats@matsdeswart.nl>
2024-11-19 01:58:29 +0100

    Styled signed out button

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -13,13 +13,13 @@ export const DashboardScreen = () => {
     return (
         <GradientPage>
             <div className="flex flex-col items-start justify-start bg-white w-1/2 p-12 rounded-lg shadow-lg mt-8 mx-auto">
-                <div className="flex justify-between items-start w-full">
-                    <img src="/logo.svg" className="max-w-[200px] mb-4" />
-                    <button onClick={() => { void signOut(firebaseAuth); }}>
-                        Sign out
+                <div className="flex justify-between items-start w-full mb-4">
+                    <img src="/logo.svg" className="max-w-[200px]" />
+                    <button className="ml-3 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-3 rounded" onClick={() => { void signOut(firebaseAuth); }}>
+                        Sign Out
                     </button>
                 </div>
-                <p className="text-xl">Welcome back careibu!</p>
+                <p className="text-xl mb-4">Welcome back careibu!</p>
                 <p>{user.email}</p>
             </div>
         </GradientPage>
