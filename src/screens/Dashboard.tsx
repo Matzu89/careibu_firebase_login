4d71e8e4d1771fd4e46ef70ca386ce9fc72d87be
Mats De Swart <mats@matsdeswart.nl>
2024-11-19 04:40:31 +0100

    Make the dashboard not look terrible on mobile

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -12,13 +12,13 @@ export const DashboardScreen = () => {
 
     return (
         <GradientPage>
-            <div className="flex flex-col items-start justify-start bg-white w-1/2 p-12 rounded-lg shadow-lg mt-8 mx-auto">
-                <div className="flex justify-between items-start w-full mb-4">
-                    <img src="/logo.svg" className="max-w-[200px]" />
+            <div className="flex flex-col items-start justify-start bg-white p-12 rounded-lg shadow-lg w-full mx-4 mt-8 md:mx-auto md:max-w-[800px]">
+                <div className="flex justify-end items-start w-full mb-8">
                     <button className="ml-3 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-3 rounded" onClick={() => { void signOut(firebaseAuth); }}>
                         Sign Out
                     </button>
                 </div>
+                <img src="/logo.svg" className="max-w-[200px]" />
                 <p className="text-xl mb-4">Welcome back careibu!</p>
                 <p>{user.email}</p>
             </div>
